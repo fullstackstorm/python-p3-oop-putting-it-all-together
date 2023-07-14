14190a7ffe896c1213469b79a5827f6cc710fc20
Stormi Anduaga-Arias <stormarias@gmail.com>
2023-07-13 22:10:38 -0700

    Practice with classes and attributes.

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,31 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    
+    def __init__(self, title = "The Shining", page_count = "450"):
+        self.set_title(title)
+        self.set_page_count(page_count)
+
+    def get_title(self):
+        return self.__title
+    
+    def set_title(self, title):
+        self.__title = title
+
+    def get_page_count(self):
+        return self.__page_count
+    
+    def set_page_count(self, page_count):
+        if isinstance(page_count, int):
+            self.__page_count = page_count
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+    title = property(get_title, set_title)
+    page_count = property(get_page_count, set_page_count)
+
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,30 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    condition = None
+
+    def __init__(self, brand="Prada", size=6):
+        self.set_brand(brand)
+        self.set_size(size)
+    
+    def set_brand(self, brand):
+        self.__brand = brand
+
+    def get_brand(self):
+        return self.__brand
+    
+    def set_size(self, size):
+        if isinstance(size, int):
+            self.__size = size
+        else:
+            print("size must be an integer")
+
+    def get_size(self):
+        return self.__size
+    
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"        
+    
+    brand = property(get_brand, set_brand)
+    size = property(get_size, set_size)
